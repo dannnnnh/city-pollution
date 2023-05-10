1aa2f92e88d67c1a714e8c862107ad0a1ac1c315
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-05-10 11:44:53 +0200

    Adding comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 //test another coom
 import React, { useState, useEffect } from "react";
+
+//Import  all needed components for recharts
 import {
   BarChart,
   Bar,
@@ -13,27 +15,39 @@ import {
 } from "recharts";
 
 function App() {
+  //This is for the graph data
   const [data, setData] = useState([]);
+
+  //Station names to replace the station codes
   const [stations, setStations] = useState([]);
+
+  //Loading state (text)
   const [loading, setLoading] = useState(true);
 
+  //Date formatter to human format (day, month, year)
   const dateFormatter = new Intl.DateTimeFormat("en-GB", {
     year: "numeric",
     month: "long",
     day: "2-digit",
   });
 
+  //Function for fetching the stations data
   const fetchStations = async () => {
     try {
       const response = await fetch("https://luftdaten.berlin.de/api/stations");
+
+      //convert the response to json
       const jsonData = await response.json();
       return jsonData;
+
+      //catch any errors
     } catch (error) {
       console.error("Error fetching station data:", error);
       return [];
     }
   };
 
+  //Function for creating a map of station codes to station names
   const createStationCodeToNameMap = (stations) => {
     const stationCodeToNameMap = {};
 
@@ -67,22 +81,31 @@ function App() {
     fetchData();
   }, []);
 
+  //If loading is true, show loading text
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  //Create a map of station codes to station names
   const stationCodeToNameMap = createStationCodeToNameMap(stations);
 
   //Function for handling the most polluted keiz text
   const findMostPollutedKeiz = (data) => {
+    //Set the most polluted keiz to empty string
     let mostPollutedKeiz = "";
     let pollutants = new Map(); // Use a Map to store unique pollutants and their values
 
+    //Loop through the data
     data.forEach((stationData) => {
+      //Loop through the station data
       stationData.data.forEach((measurement) => {
+        //If the current measurement is greater than the current max, set the current max to the current measurement
         const currentMax = pollutants.get(measurement.component);
         if (!currentMax || measurement.value > currentMax) {
+          //Set the most polluted keiz to the current station
           pollutants.set(measurement.component, measurement.value);
+
+          //if the measurement is lqi, set the most polluted keiz to the current station
           if (measurement.component === "lqi") {
             mostPollutedKeiz = stationData.station;
           }
@@ -93,22 +116,29 @@ function App() {
     return { mostPollutedKeiz, pollutants };
   };
 
+  //Get the most polluted keiz and pollutants
   const { mostPollutedKeiz, pollutants } = findMostPollutedKeiz(data);
+
+  //Format the date
   const currentDate = dateFormatter.format(new Date());
+
+  //Get the most polluted keiz name
   const mostPollutedStationName =
     stationCodeToNameMap[mostPollutedKeiz] || mostPollutedKeiz;
 
+  //Get the top 2 pollutants
   const sortedPollutants = Array.from(pollutants.entries())
     .sort((a, b) => b[1] - a[1]) // Sort pollutants by descending values
     .slice(0, 2); // Get the top 2 pollutants
 
+  //Create a string of the pollutants
   const pollutantsString = sortedPollutants
     .map(([component, value]) => `${component} (level ${value})`)
     .join(" and ");
 
   return (
     <div className="App">
-      <div style={{ margin: "8px" }}>
+      <div style={{ margin: "16px" }}>
         <h1 style={{ paddingTop: "48px", marginBottom: "16px" }}>
           💨 Berlin pollution data
         </h1>
@@ -167,6 +197,7 @@ function App() {
           flexWrap: "wrap",
         }}
       >
+        {/* Loop through the data */}
         {data.map((stationData, index) => {
           const chartData = stationData.data;
           const firstMeasurement = stationData.data[0];
